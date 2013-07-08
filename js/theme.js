e1dce3e258a24f30ddd49117e638c6df37748edf
refaktorco <refaktor.co@gmail.com>
2013-07-08 14:42:09 -0700

    -Have to convert existing theme JS to be compatible with the version that ships with jQuery update.

--- js/theme.js
+++ js/theme.js
@@ -340,56 +340,7 @@ var Core = {
 
 	},
 
-	sort: function() {
 
-		$("ul.sort-source").each(function() {
-
-			var source = $(this);
-			var destination = $("ul.sort-destination[data-sort-id=" + $(this).attr("data-sort-id") + "]");
-
-			if(destination.get(0)) {
-
-				var minParagraphHeight = 0;
-				var paragraphs = $("span.thumb-info-caption p", destination);
-
-				paragraphs.each(function() {
-					if($(this).height() > minParagraphHeight)
-						minParagraphHeight = $(this).height();
-				});
-
-				paragraphs.height(minParagraphHeight);
-
-				$(window).load(function() {
-
-					destination.isotope({
-						itemSelector: "li",
-						layoutMode : "fitRows"
-					});
-
-					source.find("a").click(function(e) {
-
-						e.preventDefault();
-
-						var $this = $(this);
-
-						source.find("li.active").removeClass("active");
-						$(this).parent().addClass("active");
-
-						destination.isotope({
-							filter: $this.parent().attr("data-option-value")
-						});
-
-						return false;
-
-					});
-
-				});
-
-			}
-
-		});
-
-	},
 
 	toggle: function() {
 
