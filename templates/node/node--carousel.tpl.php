bf1ebdfec8aab3ec34b4ded366054fa81c92311e
refaktorco <refaktor.co@gmail.com>
2013-08-07 11:02:29 -0700

    -Working on stuff for client carousel content type/View

---
+++ templates/node/node--carousel.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file node--faq.tpl.php
+ * Porto's node template for the FAQ content type.
+ */
+?>
+<li>
+	  <div class="span2">
+<?php print render($content['field_image']); ?>
+  </div>  
+  </li>
