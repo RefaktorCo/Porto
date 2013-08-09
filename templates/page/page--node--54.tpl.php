3c3de95287bd7a2230cbc8889a92c174f2b85262
refaktorco <refaktor.co@gmail.com>
2013-08-09 13:39:29 -0700

    -New custom script works and I found out you can link to # by using the node path (ex: node/54#ID)

--- templates/page/page--node--54.tpl.php
+++ templates/page/page--node--54.tpl.php
@@ -45,30 +45,7 @@
       </div>
       <?php print render($page['header_icons']); ?>
       <nav>
-        <ul class="nav nav-pills nav-main" id="mainMenu">
-							<li class="dropdown active">
-								<a data-hash class="dropdown-toggle" href="#home">
-									Home
-									<i class="icon-angle-down"></i>
-								</a>
-								<ul class="dropdown-menu">
-									<li><a href="index.html">Default Home</a></li>
-									<li><a href="index-one-page.html">One Page Website</a></li>
-								</ul>
-							</li>
-							<li>
-								<a href="#projects">Latest Projects</a>
-							</li>
-							<li>
-								<a href="#features">Features</a>
-							</li>
-							<li>
-								<a href="#team">Meet the Team</a>
-							</li>
-							<li>
-								<a href="#contact">Contact Us</a>
-							</li>
-						</ul>
+       <?php print render($page['header_menu']); ?>
       </nav>
       
     </div>  
@@ -203,12 +180,12 @@
 	</footer>
 <script type="text/javascript">
 jQuery(document).ready(function ($) {
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top -150
-    }, 500);
-    return false;
-});
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $(this.hash).offset().top -150
+	    }, 500);
+	    return false;
+	});
 });
 </script>	
 </div>	
