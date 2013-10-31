9860d9b26cca818a04452933813923836e9debbb
refaktorco <refaktor.co@gmail.com>
2013-10-31 10:44:19 -0700

    -Tweaked H1 stuff.

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -14,20 +14,26 @@
 	    <?php endif; ?>
     
       <?php if ($logo): ?>
-        <h1 class="logo">
+        <div class="logo">
 		      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
 		        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
 		      </a>
-        </h1>
+        </div>
 	    <?php endif; ?>
 	    
 	    <?php if ($site_name || $site_slogan): ?>
 	      <div id="name-and-slogan"<?php if ($disable_site_name && $disable_site_slogan) { print ' class="hidden"'; } ?>>
 	
 	        <?php if ($site_name): ?>
-	          <h1 id="site-name"<?php if ($disable_site_name) { print ' class="hidden"'; } ?>>
-	            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-	          </h1>
+	          <?php if ($title): ?>
+	            <div id="site-name"<?php if ($disable_site_name) { print ' class="hidden"'; } ?>>
+		            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+		          </div>
+	          <?php else: /* Use h1 when the content title is empty */ ?>
+		          <h1 id="site-name"<?php if ($disable_site_name) { print ' class="hidden"'; } ?>>
+		            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+		          </h1>
+	          <?php endif; ?>
 	        <?php endif; ?>
 	
 	        <?php if ($site_slogan): ?>
@@ -65,7 +71,7 @@
 				<?php endif; ?>
 				<div class="row">
 					<div class="span12">
-						<h2><?php print drupal_get_title(); ?></h2>
+						<h1><?php print drupal_get_title(); ?></h1>
 					</div>
 				</div>
 			</div>
