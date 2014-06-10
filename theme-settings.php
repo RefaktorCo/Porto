e12ab40b55f0e2b8173c67db96957a1ac3c755bf
refaktorco <refaktor.co@gmail.com>
2014-06-10 16:05:00 -0700

    -Starting twitter content type

--- theme-settings.php
+++ theme-settings.php
@@ -199,61 +199,7 @@ function porto_form_system_theme_settings_alter(&$form, &$form_state) {
         '#title' => t('Add your own CSS'),
         '#default_value' => theme_get_setting('user_css'),
       ); 
-        
-  // Twitter
-  $form['options']['twitter'] = array(
-    '#type' => 'fieldset',
-    '#title' => t('Twitter API'),
-  );    
-  
-     // Twitter App Consumer Key
-    $form['options']['twitter']['twitter_app_consumer_key'] =array(
-      '#type' => 'textfield',
-      '#title' => t('Twitter App Consumer Key'),
-      '#default_value' => theme_get_setting('twitter_app_consumer_key'),
-      '#states' => array (
-        'invisible' => array(
-          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
-        )
-      )
-    );
-    
-    // Twitter App Consumer Secret
-    $form['options']['twitter']['twitter_app_consumer_secret'] =array(
-      '#type' => 'textfield',
-      '#title' => t('Twitter App Consumer Secret'),
-      '#default_value' => theme_get_setting('twitter_app_consumer_secret'),
-      '#states' => array (
-        'invisible' => array(
-          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
-        )
-      )
-    );
-    
-    // Twitter App Access Token
-    $form['options']['twitter']['twitter_app_access_token'] =array(
-      '#type' => 'textfield',
-      '#title' => t('Twitter App Access Token'),
-      '#default_value' => theme_get_setting('twitter_app_access_token'),
-      '#states' => array (
-        'invisible' => array(
-          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
-        )
-      )
-    );
-    
-    // Twitter App Access Token Secret
-    $form['options']['twitter']['twitter_app_access_secret'] =array(
-      '#type' => 'textfield',
-      '#title' => t('Twitter App Access Token Secret'),
-      '#default_value' => theme_get_setting('twitter_app_access_secret'),
-      '#states' => array (
-        'invisible' => array(
-          'input[name="enable_twitter_feed"]' => array('checked' => FALSE)
-        )
-      )
-    );
-    
+            
   // Footer
   $form['options']['footer'] = array(
     '#type' => 'fieldset',
