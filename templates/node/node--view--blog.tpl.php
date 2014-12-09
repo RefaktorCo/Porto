99726c5d14dfb771908317a9ecaa6214cafa9ed6
corning <tylerbcunning@gmail.com>
2014-12-08 16:18:43 -0800

    -Working on views layouts/removing template files

--- templates/node/node--view--blog.tpl.php
+++ templates/node/node--view--blog.tpl.php
@@ -1,5 +1,4 @@
 <?php 
-
 if ($items = field_get_items('node', $node, 'field_image')) {
   if (count($items) == 1) {
     $image_slide = 'false';
@@ -7,12 +6,9 @@ if ($items = field_get_items('node', $node, 'field_image')) {
   elseif (count($items) > 1) {
     $image_slide = 'true';
   }
-  
-  $uid = user_load($node->uid);
-  $profile = profile2_load_by_user($uid, 'main');
 }
-
 ?>
+
 <?php if (theme_get_setting('blog_image') == 'full'): ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> post post-large blog-single-post"<?php print $attributes; ?>>
 
@@ -76,52 +72,19 @@ if ($items = field_get_items('node', $node, 'field_image')) {
 	    ?>
 	  </div>
 	  
-		  <?php if (!$page && $teaser): ?>
-	  
-	     <div class="post-meta">
-				<span class="post-meta-user"><i class="icon icon-user"></i> <?php print t('By'); ?> <?php print $name; ?> </span>
-				<?php if (render($content['field_tags'])): ?> 
-				  <span class="post-meta-tag"><i class="icon icon-tag"></i> <?php print render($content['field_tags']); ?> </span>
-				<?php endif; ?> 
-				<span class="post-meta-comments"><i class="icon icon-comments"></i> <a href="<?php print $node_url;?>/#comments"><?php print $comment_count; ?> <?php print t('Comment'); ?><?php if ($comment_count != "1" ) { echo "s"; } ?></a></span>
-				<a href="<?php print $node_url; ?>" class="btn btn-xs btn-primary pull-right"><?php echo t('Read more...'); ?></a>
-			</div>
-		
-	  
+		<?php if (!$page && $teaser): ?>
+	  <div class="post-meta">
+			<span class="post-meta-user"><i class="icon icon-user"></i> <?php print t('By'); ?> <?php print $name; ?> </span>
+			<?php if (render($content['field_tags'])): ?> 
+			  <span class="post-meta-tag"><i class="icon icon-tag"></i> <?php print render($content['field_tags']); ?> </span>
+			<?php endif; ?> 
+			<span class="post-meta-comments"><i class="icon icon-comments"></i> <a href="<?php print $node_url;?>/#comments"><?php print $comment_count; ?> <?php print t('Comment'); ?><?php if ($comment_count != "1" ) { echo "s"; } ?></a></span>
+			<a href="<?php print $node_url; ?>" class="btn btn-xs btn-primary pull-right"><?php echo t('Read more...'); ?></a>
+		</div>
 	  <?php endif; ?>
-	  
-	  <?php if(!$teaser): ?>
-	  <div class="post-block post-share">
-			<h3><i class="icon-share"></i><?php print t('Share this post'); ?></h3>
-
-			<!-- AddThis Button BEGIN -->
-			<div class="addthis_toolbox addthis_default_style ">
-				<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-				<a class="addthis_button_tweet"></a>
-				<a class="addthis_button_pinterest_pinit"></a>
-				<a class="addthis_counter addthis_pill_style"></a>
-			</div>
-			<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-50faf75173aadc53"></script>
-			<!-- AddThis Button END -->
-
-		</div>
-	  
-	  <div class="post-block post-author clearfix">
-			<h3><i class="icon-user"></i><?php print t('Author'); ?></h3>
-			<div class="thumbnail">
-			 <?php print $user_picture; ?>
-			</div>
-			<p><strong class="name"><?php print $name; ?> </strong></p>
-		    <?php if (isset($profile->field_bio['und'][0]['value'])): ?>
-          <?php print ($profile->field_bio['und'][0]['value']); ?>
-        <?php endif; ?>
-		</div>
-		<?php endif; ?>  
-  
+	    
 	</div>
   
-  <?php print render($content['comments']); ?>
-
 </article>
 <?php endif; ?>
 
